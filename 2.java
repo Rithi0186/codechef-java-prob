26cdc19d9e4ccaa63818408adf24570c31b60ca1
Rithi0186 <rithisubhasasi123@gmail.com>
2025-09-03 15:20:32 +0530

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+ Scanner sc= new Scanner(System.in);
+      
+      String word=sc.next();
+      System.out.println(word.charAt(6));
+	}
+}
